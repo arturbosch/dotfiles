4361b69e6af7554ae398acbd60b7a1dde9e5f776
Artur Bosch <arturbosch@gmx.de>
2020-05-29 12:22:28 +0200

    Enable middle and secondary touchpad clicks

--- install/0_update.sh
+++ install/0_update.sh
@@ -1,6 +1,9 @@
+#!/bin/env sh
 # copy home backup and update os
 sudo apt update && sudo apt upgrade -y
 
 # map capslock to escape
 setxkbmap -option caps:escape
+# enable middle and right click for touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
 
