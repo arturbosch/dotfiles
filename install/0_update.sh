ff93bc369b7c11e4c72d81c4e29501395e4f0982
Artur Bosch <arturbosch@gmx.de>
2020-06-16 22:57:45 +0200

    Add compatibility layer for manjaro

--- install/0_update.sh
+++ install/0_update.sh
@@ -1,11 +1,32 @@
 #!/bin/env sh
 # copy home backup and update os
-sudo apt update && sudo apt upgrade -y
+
+if (uname -a | grep -q ubuntu)
+
+then # regolith specific
+
+    sudo apt update && sudo apt upgrade -y
+
+    # enable middle and right click for touchpad
+    gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
+
+else # manjaro specific
+
+    sudo pamac update && sudo pamac upgrade
+
+fi
+
+# common part
 
 # map capslock to escape
 setxkbmap -option caps:escape
-# enable middle and right click for touchpad
-gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
 
-sudo ln -s "$HOME/dotfiles/startup.sh" /etc/init.d/startup.sh
+# enable power management
 
+if (systemctl | grep -q powertop)
+then
+    echo "powertop already configured"
+else
+    sudo sh -c "echo -e '[Unit]\nDescription=PowerTop\n\n[Service]\nType=oneshot\nRemainAfterExit=true\nExecStart=/usr/bin/powertop --auto-tune\n\n[Install]\nWantedBy=multi-user.target\n' > /etc/systemd/system/powertop.service"
+    sudo systemctl enable --now powertop.service
+fi
--- install/5_other.sh
+++ install/5_other.sh
@@ -3,11 +3,16 @@
 conf=$HOME/dotfiles
 
 # setup kak plugins
-chmod +x $conf/kak/install_kak_plug.sh $conf/kak/install_kak_plug.sh
+chmod +x $conf/kak/install_kak_plug.sh
+sh $conf/kak/install_kak_plug.sh
 
-# brave browser
-sudo apt install apt-transport-https curl
-curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-prerelease.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
-sudo apt update
-sudo apt install brave-browser-beta
+if (uname -a | grep -q ubuntu)
+then
+    # brave browser
+    sudo apt install apt-transport-https curl
+    curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-prerelease.gpg add -
+    echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+    sudo apt update
+    sudo apt install brave-browser-beta
+fi
+
