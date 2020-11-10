1f8a6d73d96f57176135726f6be54d1f824a55ce
Artur Bosch <arturbosch@gmx.de>
2020-11-10 16:24:10 +0100

    Introduce fish_plugins file

--- install/2_links.sh
+++ install/2_links.sh
@@ -3,13 +3,14 @@
 # variables
 alacritty="$HOME/.config/alacritty/alacritty.yml"
 fish="$HOME/.config/fish/conf.d/conf.fish"
+fish_plugins="$HOME/.config/fish/fish_plugins"
 kakrc="$HOME/.config/kak/kakrc"
 nvim="$HOME/.config/nvim/init.vim"
 gitconfig="$HOME/.gitconfig"
 roficonfig="$HOME/.config/rofi/config"
 
 # remove old links
-for var in $alacritty $fish $kakrc $nvim $gitconfig $roficonfig
+for var in $alacritty $fish $kakrc $nvim $gitconfig $roficonfig $fish_plugins
 do
     rm $var
 done
@@ -17,6 +18,7 @@ done
 # create links
 ln -s $HOME/dotfiles/alacritty/alacritty.yml $alacritty
 ln -s $HOME/dotfiles/fish/conf.fish $fish
+ln -s $HOME/dotfiles/fish/fish_plugins $fish_plugins
 ln -s $HOME/dotfiles/kak/kakrc $kakrc
 ln -s $HOME/dotfiles/nvim/init.vim $nvim
 ln -s $HOME/dotfiles/.gitconfig $gitconfig
