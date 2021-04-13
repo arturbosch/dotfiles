17ea6ba19e315a868752f6d4a7bbe2c9ceb1ce8d
Artur Bosch <arturbosch@gmx.de>
2021-04-13 12:14:40 +0200

    Update i3 config for latest manjaro

--- install/3_software.sh
+++ install/3_software.sh
@@ -5,7 +5,7 @@ if (uname -a | grep -q pop-os)
 then
     system="pop-os"
     install="sudo apt install"
-    additional_software="printer-driver-gutenprint scrot pcmanfm tldr-py neovim-qt pavucontrol compton xautolock pasystray libnotify-bin brightnessctl python3-pip"
+    additional_software="printer-driver-gutenprint scrot pcmanfm tldr-py neovim-qt pavucontrol compton xautolock pasystray libnotify-bin brightnessctl python3-pip arp-scan"
 
     sudo apt remove cmdtest
     sudo apt remove yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -19,8 +19,7 @@ else
 fi
 
 ## install common software
-### archived: jekyll autojump
-system_tools="htop powertop tlp curl wget zip unzip git fish alacritty fzf tig meld tk tcl tldr asciinema rofi timewarrior neofetch moc"
+system_tools="htop powertop tlp curl wget zip unzip git fish alacritty fzf tig meld tk tcl tldr asciinema rofi timewarrior neofetch moc brightnessctl"
 office_tools="thunderbird gimp virtualbox steam"
 dev_tools="neovim kakoune hugo cargo nodejs yarn ruby"
 
