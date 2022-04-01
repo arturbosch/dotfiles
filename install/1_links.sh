566e4c94ab88bb0eb43316e1e9ef9d04f825c0e1
Artur Bosch <abosch@posteo.de>
2022-04-01 14:14:17 +0200

    Add lf

--- install/1_links.sh
+++ install/1_links.sh
@@ -2,6 +2,25 @@
 
 dotfiles="$HOME/dotfiles"
 
+# mkdirs
+
+mkdir -p $HOME/.config/lf
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/qtile
+mkdir -p $HOME/.config/rofi
+mkdir -p $HOME/.config/kak/plugins
+mkdir -p $HOME/.config/alacritty
+mkdir -p $HOME/.config/fish/conf.d
+mkdir -p $HOME/.config/sway
+mkdir -p $HOME/bin
+mkdir -p $HOME/dotfiles
+mkdir -p $HOME/files
+mkdir -p $HOME/git/repos
+mkdir -p $HOME/git/test
+mkdir -p $HOME/git/system
+mkdir -p $HOME/scripts
+mkdir -p $HOME/tools
+
 # variables
 alacritty="$HOME/.config/alacritty/alacritty.yml"
 fish="$HOME/.config/fish/conf.d/conf.fish"
@@ -13,9 +32,10 @@ roficonfig="$HOME/.config/rofi/config"
 qtileconfig="$HOME/.config/qtile/config.py"
 bashrcd="$HOME/.bashrc.d"
 swayconfig="$HOME/.config/sway/config"
+lfrc="$HOME/.config/lf/lfrc"
 
 # remove old links
-for var in $alacritty $fish $kakrc $nvim $gitconfig $roficonfig $fish_plugins $qtileconfig $swayconfig
+for var in $alacritty $fish $kakrc $nvim $gitconfig $roficonfig $fish_plugins $qtileconfig $swayconfig $lfrc
 do
     rm $var
 done
@@ -30,6 +50,9 @@ ln -s $dotfiles/.gitconfig $gitconfig
 ln -s $dotfiles/rofi/config $roficonfig
 ln -s $dotfiles/qtile/config.py $qtileconfig
 ln -s $dotfiles/sway/config $swayconfig
+ln -s $dotfiles/lf/lfrc $lfrc
+chmod +x $dotfiles/lf/previewer.sh
+
 if [ ! -d $bashrcd ]
 then
     ln -s $dotfiles/.bashrc.d $bashrcd
