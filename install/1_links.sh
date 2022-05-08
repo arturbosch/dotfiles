459fc27b1b74d5a6b42955628c80e69fe4c0e881
Artur Bosch <abosch@posteo.de>
2022-05-08 19:14:00 +0200

    [install] simplify links script

--- install/1_links.sh
+++ install/1_links.sh
@@ -12,6 +12,7 @@ mkdir -p $HOME/.config/kak/plugins
 mkdir -p $HOME/.config/alacritty
 mkdir -p $HOME/.config/fish/conf.d
 mkdir -p $HOME/.config/sway
+mkdir -p $HOME/.config/waybar
 mkdir -p $HOME/bin
 mkdir -p $HOME/dotfiles
 mkdir -p $HOME/files
@@ -34,28 +35,21 @@ bashrcd="$HOME/.bashrc.d"
 swayconfig="$HOME/.config/sway/config"
 lfrc="$HOME/.config/lf/lfrc"
 
-# remove old links
-for var in $alacritty $fish $kakrc $nvim $gitconfig $roficonfig $fish_plugins $qtileconfig $swayconfig $lfrc
-do
-    rm $var
-done
-
 # create links
-ln -s $dotfiles/alacritty/alacritty.yml $alacritty
-ln -s $dotfiles/fish/conf.fish $fish
-ln -s $dotfiles/fish/fish_plugins $fish_plugins
-ln -s $dotfiles/kak/kakrc $kakrc
-ln -s $dotfiles/nvim/init.vim $nvim
-ln -s $dotfiles/.gitconfig $gitconfig
-ln -s $dotfiles/rofi/config $roficonfig
-ln -s $dotfiles/qtile/config.py $qtileconfig
-ln -s $dotfiles/sway/config $swayconfig
-ln -s $dotfiles/lf/lfrc $lfrc
+ln -sf $dotfiles/alacritty/alacritty.yml $alacritty
+ln -sf $dotfiles/fish/conf.fish $fish
+ln -sf $dotfiles/fish/fish_plugins $fish_plugins
+ln -sf $dotfiles/kak/kakrc $kakrc
+ln -sf $dotfiles/nvim/init.vim $nvim
+ln -sf $dotfiles/.gitconfig $gitconfig
+ln -sf $dotfiles/rofi/config $roficonfig
+ln -sf $dotfiles/qtile/config.py $qtileconfig
+ln -sf $dotfiles/sway/config $swayconfig
+ln -sf $dotfiles/waybar $HOME/.config
+ln -sf $dotfiles/lf/lfrc $lfrc
 chmod +x $dotfiles/lf/previewer.sh
 
 if [ ! -d $bashrcd ]
 then
-    ln -s $dotfiles/.bashrc.d $bashrcd
-else
-    echo "$bashrcd already present"
+    ln -sf $dotfiles/.bashrc.d $bashrcd
 fi
