286a7a7db370e7c31a12b6c1c4777e4bb2e08b33
Artur Bosch <abosch@posteo.de>
2022-05-08 20:22:29 +0200

    [sway] set current desktop to Unity for appindicators to work

--- sway_start.sh
+++ sway_start.sh
@@ -4,7 +4,7 @@ export QT_QPA_PLATFORM=wayland
 export QT_QPA_PLATFORMTHEME=qt5ct
 export _JAVA_AWT_WM_NONREPARENTING=1
 export GDK_BACKEND=wayland
-export XDG_CURRENT_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=Unity
 
 if [[ -f /usr/bin/sway ]]; then
 	sway
