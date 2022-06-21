39330d23cd3730ef6c9cdec2916117737d00233d
Artur Bosch <abosch@posteo.de>
2022-06-21 21:35:25 +0200

    [flatpak] more flatpak tools

--- install/3_flatpak.sh
+++ install/3_flatpak.sh
@@ -15,4 +15,16 @@ flatpak install flathub io.freetubeapp.FreeTube # Youtube Frontend
 flatpak install flathub org.gnome.Boxes # Virtualization
 flatpak install flathub org.onlyoffice.desktopeditors # Office Suite
 flatpak install flathub com.github.huluti.Curtail # Image Minimizer
+flatpak install flathub io.github.seadve.Kooha # Screen Recorder
+flatpak install flathub org.gnome.gitlab.somas.Apostrophe # Backup Solution
+flatpak install flathub org.gnome.World.PikaBackup # Backup Solution
+flatpak install fedora org.gnome.Connections # Backup Solution
+flatpak install flathub org.gnome.Solanum # Backup Solution
+flatpak install flathub com.github.tchx84.Flatseal # Backup Solution
+flatpak install flathub com.github.liferooter.textpieces # Backup Solution
+flatpak install flathub com.github.jeromerobert.pdfarranger # Merge an edit PDFs
+flatpak install flathub org.gnome.World.Secrets # Password Store
+flatpak install flathub com.lakoliu.Furtherance # Time Tracking
 
+## uses old gnome platform and needs all latex package: 5gb+
+# flatpak install flathub org.cvfosammmm.Setzer # latex editor
