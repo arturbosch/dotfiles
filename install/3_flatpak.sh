79ca7e392e9d15fd161f791c3d77a94ab2e246d0
Artur Bosch <abosch@posteo.de>
2022-07-06 13:49:38 +0200

    [flatpak] update

--- install/3_flatpak.sh
+++ install/3_flatpak.sh
@@ -6,6 +6,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 ## Apps
 
 flatpak install flathub com.belmoussaoui.Authenticator # Two Factory Auth
+flatpak install flathub com.github.neithern.g4music # Music Player
 flatpak install flathub io.bassi.Amberol # Music Player
 flatpak install fedora com.github.johnfactotum.Foliate # E-Book Reader
 flatpak install flathub com.gitlab.newsflash # RSS Reader
@@ -16,15 +17,16 @@ flatpak install flathub org.gnome.Boxes # Virtualization
 flatpak install flathub org.onlyoffice.desktopeditors # Office Suite
 flatpak install flathub com.github.huluti.Curtail # Image Minimizer
 flatpak install flathub io.github.seadve.Kooha # Screen Recorder
-flatpak install flathub org.gnome.gitlab.somas.Apostrophe # Backup Solution
+flatpak install flathub org.gnome.gitlab.somas.Apostrophe # Markdown Editor
 flatpak install flathub org.gnome.World.PikaBackup # Backup Solution
-flatpak install fedora org.gnome.Connections # Backup Solution
-flatpak install flathub org.gnome.Solanum # Backup Solution
-flatpak install flathub com.github.tchx84.Flatseal # Backup Solution
-flatpak install flathub com.github.liferooter.textpieces # Backup Solution
+flatpak install fedora org.gnome.Connections # Remote Desktop
+flatpak install flathub org.gnome.Solanum # Pomodoro Timer
+flatpak install flathub com.github.tchx84.Flatseal # Manage Permissions
+flatpak install flathub com.github.liferooter.textpieces # Text Manipulation
 flatpak install flathub com.github.jeromerobert.pdfarranger # Merge an edit PDFs
 flatpak install flathub org.gnome.World.Secrets # Password Store
 flatpak install flathub com.lakoliu.Furtherance # Time Tracking
+flatpak install flathub com.github.philip_scott.notes-up # Notes app
 
 ## uses old gnome platform and needs all latex package: 5gb+
 # flatpak install flathub org.cvfosammmm.Setzer # latex editor
