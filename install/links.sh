daa390b47b1b96e2397b109435a8049e294c8e07
Artur Bosch <abosch@posteo.de>
2023-01-05 21:01:49 +0100

    [install] add links to foot and helix

--- install/links.sh
+++ install/links.sh
@@ -8,30 +8,30 @@ confdir=$HOME/.config
 for dir in $confdir/kak/plugins \
             $confdir/alacritty \
             $confdir/fish/conf.d \
-            $HOME/bin $HOME/dotfiles $HOME/files $HOME/git/repos $HOME/git/test $HOME/git/system $HOME/scripts $HOME/tools
+            $HOME/bin \
+            $HOME/dotfiles \
+            $HOME/files \
+            $HOME/git/repos \
+            $HOME/git/test \
+            $HOME/git/system \
+            $HOME/scripts \
+            $HOME/tools \
+            $confdir/foot \
+            $confdir/helix
 do
     mkdir -p $dir
 done
 
-# variables
-alacritty="$confdir/alacritty/alacritty.yml"
-fish="$confdir/fish/conf.d/conf.fish"
-fish_plugins="$confdir/fish/fish_plugins"
-kakrc="$confdir/kak/kakrc"
-nvim="$confdir/nvim/init.vim"
-gitconfig="$HOME/.gitconfig"
-roficonfig="$confdir/rofi/config"
-qtileconfig="$confdir/qtile/config.py"
-bashrcd="$HOME/.bashrc.d"
-swayconfig="$confdir/sway/config"
-lfrc="$confdir/lf/lfrc"
-
 # create links
-ln -sf $dotfiles/alacritty/alacritty.yml $alacritty
-ln -sf $dotfiles/fish/conf.fish $fish
-ln -sf $dotfiles/fish/fish_plugins $fish_plugins
-ln -sf $dotfiles/kak/kakrc $kakrc
-ln -sf $dotfiles/.gitconfig $gitconfig
+
+ln -sf $dotfiles/alacritty/alacritty.yml $confdir/alacritty/alacritty.yml
+ln -sf $dotfiles/fish/conf.fish $confdir/fish/conf.d/conf.fish
+ln -sf $dotfiles/fish/fish_plugins $confdir/fish/fish_plugins
+ln -sf $dotfiles/foot/foot.ini $confdir/foot/foot.ini
+ln -sf $dotfiles/helix/config.toml $confdir/helix/config.toml
+ln -sf $dotfiles/kak/kakrc $confdir/kak/kakrc
+ln -sf $dotfiles/.gitconfig $HOME/.gitconfig
+ln -sf $dotfiles/.gitignore $HOME/.gitignore
 
 if [ ! -d $bashrcd ]
 then
