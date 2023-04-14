71e79aceea28e27faa350f90749110fde74ee289
Artur Bosch <abosch@posteo.de>
2023-04-14 19:21:26 +0200

    [gnome] remove unneeded extensions in gnome 44

--- install/gnome_extensions.sh
+++ install/gnome_extensions.sh
@@ -3,12 +3,10 @@
 array=(
 https://extensions.gnome.org/extension/3193/blur-my-shell/
 https://extensions.gnome.org/extension/5489/search-light/
-https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
 https://extensions.gnome.org/extension/3968/improved-workspace-indicator/
 https://extensions.gnome.org/extension/1319/gsconnect/
 https://extensions.gnome.org/extension/517/caffeine/ 
 https://extensions.gnome.org/extension/5470/weather-oclock/
-https://extensions.gnome.org/extension/5446/quick-settings-tweaker/
 https://extensions.gnome.org/extension/1732/gtk-title-bar/
 )
 
