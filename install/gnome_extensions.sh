aaae505ffe6cc6aa7782bba2102149aa5d696f77
Artur Bosch <abosch@posteo.de>
2023-08-12 22:20:39 +0200

    [gnome] replacements for gtk titlebar, workspace indicators and more extensions

--- install/gnome_extensions.sh
+++ install/gnome_extensions.sh
@@ -2,12 +2,12 @@
 
 array=(
 https://extensions.gnome.org/extension/3193/blur-my-shell/
-https://extensions.gnome.org/extension/5489/search-light/
-https://extensions.gnome.org/extension/3968/improved-workspace-indicator/
 https://extensions.gnome.org/extension/1319/gsconnect/
-https://extensions.gnome.org/extension/517/caffeine/ 
 https://extensions.gnome.org/extension/5470/weather-oclock/
-https://extensions.gnome.org/extension/1732/gtk-title-bar/
+https://extensions.gnome.org/extension/615/appindicator-support/
+https://extensions.gnome.org/extension/16/auto-move-windows/
+https://extensions.gnome.org/extension/5347/dotspaces/
+https://extensions.gnome.org/extension/1287/unite/
 )
 
 for i in "${array[@]}"
