32ff4ed3b988644afee235dbd2e3d46f896ba47e
Artur Bosch <abosch@posteo.de>
2024-06-06 22:26:32 +0200

    [flatpak] rework flatpak install script

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,30 +1,32 @@
 #!/bin/env sh
 
-## Install flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+## Add flathub repository if needed
+## flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 ## Apps
+apps=(
+com.belmoussaoui.Authenticator # Two Factor Auth
+com.github.neithern.g4music # Music Player
+com.github.johnfactotum.Foliate # E-Book Reader
+com.gitlab.newsflash # RSS Reader
+com.valvesoftware.Steam # Steam Games
+com.github.Matoking.protontricks # Download missing dll's
+io.freetubeapp.FreeTube # Youtube Frontend
+org.gnome.Boxes # Virtualization
+com.github.huluti.Curtail # Image Minimizer
+org.gnome.Connections # Remote Desktop
+com.github.tchx84.Flatseal # Manage Permissions
+com.github.liferooter.textpieces # Text Manipulation
+com.github.jeromerobert.pdfarranger # Merge an edit PDFs
+org.gnome.World.Secrets # Password Store
+md.obsidian.Obsidian # Notes / Wiki
+com.nextcloud.desktopclient.nextcloud # Nextcloud Client
+org.x.Warpinator # Local Network File Sharing
+org.cvfosammmm.Setzer # latex editor
+)
 
-inst="flatpak install -y"
-
-$inst flathub com.belmoussaoui.Authenticator # Two Factory Auth
-$inst flathub com.github.neithern.g4music # Music Player
-$inst fedora com.github.johnfactotum.Foliate # E-Book Reader
-$inst flathub com.gitlab.newsflash # RSS Reader
-$inst flathub com.valvesoftware.Steam # Steam Games
-$inst flathub com.github.Matoking.protontricks # Download missing dll's
-$inst flathub io.freetubeapp.FreeTube # Youtube Frontend
-$inst flathub org.gnome.Boxes # Virtualization
-$inst flathub com.github.huluti.Curtail # Image Minimizer
-$inst flathub org.gnome.World.PikaBackup # Backup Solution
-$inst fedora org.gnome.Connections # Remote Desktop
-$inst flathub com.github.tchx84.Flatseal # Manage Permissions
-$inst flathub com.github.liferooter.textpieces # Text Manipulation
-$inst flathub com.github.jeromerobert.pdfarranger # Merge an edit PDFs
-$inst flathub org.gnome.World.Secrets # Password Store
-$inst flathub md.obsidian.Obsidian # Notes / Wiki
-$inst flathub com.nextcloud.desktopclient.nextcloud # Nextcloud Client
-$inst flathub org.x.Warpinator # Local Network File Sharing
-
-## uses old gnome platform and needs all latex package: 5gb+
-$inst flathub org.cvfosammmm.Setzer # latex editor
+for app in "${apps[@]}"
+do
+  echo $app
+  flatpak install flathub -y $app
+done
