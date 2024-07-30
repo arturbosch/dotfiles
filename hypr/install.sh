655402477b106ddbf94666c408756329fad0bc81
Artur Bosch <abosch@posteo.de>
2024-07-30 22:44:00 +0200

    [hypr] configure portals and start apps

--- hypr/install.sh
+++ hypr/install.sh
@@ -1,3 +1,5 @@
 #!/bin/env bash
 sudo dnf copr enable solopasha/hyprland
-sudo dnf install hyprland hyprpaper hyprlock waybar fuzzel fish foot wlogout polkit-gnome aylurs-gtk-shell
+sudo dnf install hyprland hyprpaper hyprlock fish foot wlogout polkit-gnome aylurs-gtk-shell
+sudo dnf remove xdg-desktop-portal-wlr
+sudo dnf install xdg-desktop-portal-hyprland
