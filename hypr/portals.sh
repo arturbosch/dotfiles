d089664bd7052ae42541e9c6097eafbd9f14c680
Artur Bosch <abosch@posteo.de>
2024-08-03 22:20:23 +0200

    [hypr] use wlr portal as it seems more reliable on my intel machines

--- hypr/portals.sh
+++ hypr/portals.sh
@@ -4,6 +4,6 @@ killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall -e xdg-desktop-portal-gnome
 killall xdg-desktop-portal
-/usr/libexec/xdg-desktop-portal-hyprland &
+/usr/libexec/xdg-desktop-portal-wlr &
 sleep 4
 /usr/libexec/xdg-desktop-portal &
